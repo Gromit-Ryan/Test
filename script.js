eb704847a67d7c7fb2e12e36232bcccc1e4c74f6
Gromit <eversoryan@gmail.com>
2022-06-04 14:06:46 -0600

    testing code

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const musicMatch = function() {
 
     const apiKey = "70f48e3a238075767ef29e2921e77a8b";
 
-    fetch(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/artist.search?apikey=${apiKey}&q_artist=${artistName}&page_size=10&format=json`, {
+    fetch(`https://api.musixmatch.com/ws/1.1/artist.search?apikey=${apiKey}&q_artist=${artistName}&page_size=10&format=json`, {
         method: 'GET'
     })
     .then(function(response) {
